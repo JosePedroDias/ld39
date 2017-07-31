53fa6982aab874ce733c28f1c56a6bbcf9e6dfe9
António Silva <antonio.josilva@gmail.com>
2017-07-31 14:36:27 +0100

    Switched level order (2 is now 3). Little tweaks to appearance and decrease in difficulty.

--- assets/level/2.js
+++ assets/level/2.js
@@ -3,106 +3,110 @@
 
   const a = [];
 
-  item(a, 300, 0, "coin");
-  item(a, 400, 75, "coin");
-  item(a, 500, 150, "coin");
-  item(a, 600, 75, "coin");
-  item(a, 700, 0, "coin");
-  item(a, 800, 75, "coin");
-  item(a, 900, 150, "coin");
-  item(a, 1000, 75, "coin");
-  item(a, 1100, 0, "coin");
-  item(a, 1200, -75, "coin");
-  item(a, 1300, -150, "coin");
-
-
-  item(a, 1600, 0, "coin");
-
-  item(a, 1800, 200, "coin");
-
-  item(a, 2200, -200, "coin");
-
-
-  item(a, 2750, 100, "gas");
-
-
-  both(a, 3500, 0, 400, 200, "stone");
-
-  
-  item(a, 3300, 0, "coin");
-  item(a, 3400, 0, "coin");
-  item(a, 3500, 0, "coin");
-  item(a, 3600, 0, "coin");
-  item(a, 3700, 0, "coin");
-
-  item(a, 3800, 0, "coin");
-  item(a, 4100, 200, "coin");
-
-  item(a, 4450, -250, "coin");
-
-
-  item(a, 4700, -250, "gas");
-
   // Y GOES DOWN (NEGATIVE IS HIGH)
 
-  //both(a, 600, 0, 100, 200, "earth");
-  item(a, 5300, 0, "coin");
-  item(a, 5400, -75, "coin");
-  item(a, 5500, -150, "coin");
-  item(a, 5600, -75, "coin");
-  item(a, 5700, 0, "coin");
-  item(a, 5800, -75, "coin");
-  item(a, 5900, -150, "coin");
-  item(a, 6000, -75, "coin");
+  both(a, 600, 0, 100, 200, "earth");
+  item(a, 300, 0, "coin");
+  item(a, 600, 0, "coin");
+  item(a, 900, 0, "coin");
+
+  below(a, 1200, -100, 100, "earth");
+
+  item(a, 1200, 85, "coin");
+
+  above(a, 1600, 100, 100, "earth");
+
+  item(a, 1600, -50, "coin");
+
+  below(a, 2000, -100, 100, "earth");
+
+  item(a, 2000, 85, "coin");
+
+  above(a, 2400, 100, 100, "earth");
+
+  item(a, 2400, -50, "coin");
+
+  both(a, 2800, 0, 100, 200, "earth");
+  item(a, 2800, 0, "gas");
+
+  //down
+  item(a, 3200, -85, "coin");
+  item(a, 3300, 0, "coin");
+  item(a, 3400, 85, "coin");
+  //up
+  item(a, 3500, 0, "coin");
+  item(a, 3600, -85, "coin");
+  item(a, 3700, -170, "coin");
+  above(a, 3600, 120, 100, "earth");
+  above(a, 3700, 70, 100, "stone");
+  above(a, 3800, 20, 100, "lava");
+  above(a, 3900, 70, 100, "stone");
+  above(a, 4000, 120, 100, "earth");
+  item(a, 3800, -170, "coin");
+  item(a, 3900, -170, "coin");
+  //down
+  item(a, 4000, -85, "coin");
+  item(a, 4100, 0, "coin");
+  item(a, 4200, 85, "coin");
+  item(a, 4300, 170, "coin");
+  //item(a, 4400, 170, "coin");
+  //up
+  item(a, 4400, 85, "coin");
+  item(a, 4500, 0, "coin");
+  item(a, 4600, -85, "coin");
+  above(a, 4500, 200, 100, "earth");
+  above(a, 4600, 100, 100, "earth");
+  above(a, 4700, 0, 100, "earth");
+  above(a, 4800, -50, 100, "earth");
+  above(a, 4900, -100, 100, "earth");
+  item(a, 4700, -170, "coin");
+  item(a, 4800, -170, "coin");
+  item(a, 4900, -170, "coin");
+  item(a, 5000, -170, "coin");
+
+  item(a, 5400, 0, "gas");
+  item(a, 5100, -170, "coin");
+  item(a, 5200, -170, "coin");
+  item(a, 5300, -170, "coin");
+
+  //down
+  item(a, 5400, -85, "coin");
+  item(a, 5500, 0, "coin");
+  item(a, 5600, 85, "coin");
+  item(a, 5700, 170, "coin");
+  item(a, 5800, 170, "coin");
+  item(a, 5900, 85, "coin");
+  item(a, 6000, 0, "coin");
   item(a, 6100, 0, "coin");
-  item(a, 6200, 75, "coin");
-  item(a, 6300, 150, "coin");
-  both(a, 6600, 0, 100, 200, "stone");
-  item(a, 6600, 0, "coin");
-  both(a, 6700, 0, 100, 400, "lava");
-  both(a, 6800, 0, 100, 400, "lava");
+  item(a, 6200, 0, "coin");
+  item(a, 6300, 0, "coin");
 
-  both(a, 6900, 0, 100, 200, "stone");
-  item(a, 6900, 0, "coin");
+  // 7000
+  both(a, 7000, 150, 100, 200, "earth");
+  item(a, 7000, 150, "coin");
 
-  both(a, 7000, 0, 100, 400, "lava");
-  both(a, 7100, 0, 100, 400, "lava");
+  both(a, 7400, 0, 100, 200, "earth");
+  item(a, 7400, 0, "coin");
 
-  both(a, 7200, 0, 100, 200, "stone");
-  item(a, 7200, 0, "coin");
+  both(a, 7800, -150, 100, 200, "earth");
+  item(a, 7800, -150, "coin");
 
-  both(a, 7300, 0, 100, 400, "lava");
-  both(a, 7400, 0, 100, 400, "lava");
+  both(a, 8200, 0, 100, 200, "earth");
+  item(a, 8200, 0, "coin");
 
-  both(a, 7500, -50, 100, 200, "lava");
-  item(a, 7500, -50, "coin");
-  both(a, 7600, -50, 100, 400, "stone");
-  both(a, 7700, -50, 100, 400, "lava");
-  item(a, 7700, 0, "coin");
-  both(a, 7800, -50, 100, 400, "lava");
-  item(a, 7800, -50, "coin");
-  both(a, 7900, -50, 100, 400, "stone");
-  item(a, 7900, -50, "coin");
-  item(a, 8000, -50, "coin");
-  item(a, 8100, -50, "coin");
-  item(a, 8200, 50, "coin");
-  both(a, 8000, -50, 100, 400, "earth");
-  both(a, 8100, 0, 100, 300, "earth");
-  both(a, 8200, 0, 100, 450, "earth");
-  both(a, 8300, 225, 100, 400, "earth");
+  both(a, 8800, 0, 100, 200, "earth");
+  item(a, 8800, 0, "coin");
 
-  item(a, 8700, -100, "coin");
-  item(a, 8800, -150, "coin");
+  both(a, 9400, 0, 100, 200, "earth");
+  item(a, 9400, 0, "gas");
 
-  item(a, 9150, 200, "gas");
+  both(a, 10000, -150, 100, 200, "earth");
+  item(a, 10000, -150, "coin");
 
-  item(a, 9400, -180, "coin");  
+  both(a, 10600, -150, 100, 200, "earth");
+  item(a, 10600, -150, "coin");
 
-  final(a, 9700, 100);
-
-  
-
-  
+  final(a, 11000, 100);
 
   window.levels["2"] = a;
 })();
